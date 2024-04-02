588886f532297921f591411ccee02ac1e73b06e7
m-ji97 <tv5035@naver.com>
2024-04-02 20:49:38 +0900

    240402 프로젝트

--- src/views/Point/PointUse.vue
+++ src/views/Point/PointUse.vue
@@ -1,64 +1,93 @@
 <template>
     <div>
         <div id="wrap">
-
-            <AppHeader/>
-
-            <div id="container" class="clearfix">
-
-                <div id="content">
-                    <div>
-                        <h1>포인트사용</h1>
-
-                        <p>휴대폰번호 <input type="number" inputmode="numeric" class="input-number"/></p>
-                        <p> 생년월일  <input type="number" inputmode="numeric" class="input-number-password"/></p>
+            <AppHeader />
+            <div id="app">
+                <div id="input-container">
+                    <input type="text" v-model="phoneNumber" placeholder="010-0000-0000">
+                    <pre></pre>
+                    <input id="birth" type="text" v-model="dateInput" placeholder="YYYYMMDD">
                     
-                    
-                        <button id="n_button" style="width: 50px;">1</button>
-                        <button id="n_button" style="width: 50px;">2</button>
-                        <button id="n_button" style="width: 50px;">3</button>
-                        <br>
-                        <button id="n_button" style="width: 50px;">4</button>
-                        <button id="n_button" style="width: 50px;">5</button>
-                        <button id="n_button" style="width: 50px;">6</button>
-                        <br>
-                        <button id="n_button" style="width: 50px;">7</button>
-                        <button id="n_button" style="width: 50px;">8</button>
-                        <button id="n_button" style="width: 50px;">9</button>
-                        <br>
-                        <button id="n_button" style="width: 50px;">0</button>
-                        <button id="n_button" style="width: 50px;">지움</button>
-                        <button id="n_button" style="width: 50px;">ㅁ</button>
-                    </div>
-
+                    <ModalView v-if="isModalViewed" @close-modal="isModalViewed = false">
+                        <FindContentView></FindContentView>
+                    </ModalView>
+                    <button id="search-button" @click="isModalViewed=true">포인트조회</button>
                 </div>
 
+                <div class="dial-container">
+                    <div class="dial-button" @click="appendNumber(1)">1</div>
+                    <div class="dial-button" @click="appendNumber(2)">2</div>
+                    <div class="dial-button" @click="appendNumber(3)">3</div>
+                    <br>
+                    <div class="dial-button" @click="appendNumber(4)">4</div>
+                    <div class="dial-button" @click="appendNumber(5)">5</div>
+                    <div class="dial-button" @click="appendNumber(6)">6</div>
+                    <br>
+                    <div class="dial-button" @click="appendNumber(7)">7</div>
+                    <div class="dial-button" @click="appendNumber(8)">8</div>
+                    <div class="dial-button" @click="appendNumber(9)">9</div>
+                    <br>
+                    <div class="dial-button" @click="appendNumber(0)">0</div>
+                    <div class="dial-button" @click="deleteLastDigit">지움</div>
+                    <div class="dial-button" @click="clearPhoneNumberMethod">전체삭제</div>
+                </div>
+
+                <div>
+                    <router-link to="/" id="return-button">돌아가기</router-link>
+                </div>
             </div>
-
-            <AppFooter/>
-
+            <AppFooter />
         </div>
     </div>
 </template>
 
-
 <script>
-import AppFooter from '@/components/AppFooter.vue';
-import AppHeader from '@/components/AppHeader.vue';
-
+import "@/assets/css/FindView.css";
+import "@/assets/css/PointUse.css";
+import AppHeader from "@/components/AppHeader.vue"
+import AppFooter from "@/components/AppFooter.vue"
+import ModalView from "@/components/ModalView.vue";
+import FindContentView from "@/components/FindContentView.vue";
 
 export default {
-    name: "PointUse",
+    name: "FindView",
     components: {
         AppHeader,
         AppFooter,
+        ModalView,
+        FindContentView,
     },
     data() {
-        return {};
+        return {
+            isModalViewed: false,
+            phoneNumber: '',
+            dateInput: '',
+            reservationNumber: '',
+        };
     },
-    methods: {},
+    methods: {
+        appendNumber(num) {
+            if (this.phoneNumber.length < 11) {
+                this.phoneNumber += num;
+            } else if (this.dateInput.length < 8) {
+                this.dateInput += num;
+            }
+        },
+        deleteLastDigit() {
+            if (this.dateInput.length > 0) {
+                this.dateInput = this.dateInput.slice(0, -1);
+            } else if (this.phoneNumber.length > 0) {
+                this.phoneNumber = this.phoneNumber.slice(0, -1);
+            }
+        },
+        clearPhoneNumberMethod() {
+            this.phoneNumber = ''; // 휴대폰 번호를 초기화합니다.
+            this.dateInput = ''; // 휴대폰 번호를 초기화합니다.
+        },
+    },
+    created() {
+    }
 };
 </script>
-
 
 <style></style>
